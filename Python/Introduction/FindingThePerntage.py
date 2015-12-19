557e43be230f8e8e774389cb3f64a3932bbffedf
Haoxuan Li <lihaoxuan6@gmail.com>
2015-12-19 21:22:52 +0800

    Delete FindingThePerntage.py

--- Python/Introduction/FindingThePerntage.py
+++
@@ -1,8 +0,0 @@
-dict = {}
-n = int(raw_input())
-for i in range(0, n):
-    raw = raw_input()
-    atom = raw.split(" ")
-    dict[atom[0]] = (float(atom[1]) + float(atom[2]) + float(atom[3])) / 3.0
-q = raw_input()
-print("%.2f" %dict[q])
